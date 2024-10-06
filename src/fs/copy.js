003518ab25e2114662ac81834e2b8351f16f7a1c
Tatyana Gulevich <t.hulevich@severex.io>
2024-10-07 02:30:33 +0300

    Add fs tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from "fs";
+
+const SOURCE_DIR = "src/fs/files";
+const DEST_DIR = "src/fs/files_copy";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const existedSourceDir = fs.existsSync(SOURCE_DIR);
+    const existedDestDir = fs.existsSync(DEST_DIR);
+    if (!existedSourceDir || existedDestDir) {
+      throw "FS operation failed";
+    } else {
+      fs.mkdirSync(DEST_DIR);
+
+      fs.readdir(SOURCE_DIR, async function (err, filenames) {
+        if (err) {
+          throw "FS operation failed";
+        }
+
+        for await (const file of filenames) {
+          fs.copyFile(`${SOURCE_DIR}/${file}`, `${DEST_DIR}/${file}`, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+
+const OLD_FILE_NAME = "src/fs/files/wrongFilename.txt";
+const NEW_FILE_NAME = "src/fs/files/properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const existedOldFile = fs.existsSync(OLD_FILE_NAME);
+    const existedNewFile = fs.existsSync(NEW_FILE_NAME);
+    if (!existedOldFile || existedNewFile) {
+      throw "FS operation failed";
+    } else {
+      fs.rename(OLD_FILE_NAME, NEW_FILE_NAME, function (err) {
+        if (err) throw err;
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await rename();
