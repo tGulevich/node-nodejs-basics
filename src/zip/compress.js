05c6f395170a5abc9ce6845eec65b59d66910b9f
Tatyana Gulevich <t.hulevich@severex.io>
2024-10-07 14:35:56 +0300

    Add zlib and esm tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import zlib from "zlib";
+
+const SOURCE_FILE = "src/zip/files/fileToCompress.txt";
+const DEST_FILE = "src/zip/files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = zlib.createGzip();
+
+    const rs = fs.createReadStream(SOURCE_FILE);
+    const ws = fs.createWriteStream(DEST_FILE);
+
+    rs.pipe(gzip)
+      .pipe(ws)
+      .on("error", (err) => {
+        throw `Error:", ${err}`;
+      });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import zlib from "zlib";
+
+const SOURCE_FILE = "src/zip/files/archive.gz";
+const DEST_FILE = "src/zip/files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const unzip = zlib.createGunzip();
+
+    const rs = fs.createReadStream(SOURCE_FILE);
+    const ws = fs.createWriteStream(DEST_FILE);
+
+    rs.pipe(unzip)
+      .pipe(ws)
+      .on("error", (err) => {
+        throw `Error:", ${err}`;
+      });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
