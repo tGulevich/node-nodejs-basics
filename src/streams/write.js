e5d3226b7e04e8ae7d7d6d09644c3aa272b05d7f
Tatyana Gulevich <t.hulevich@severex.io>
2024-10-07 03:30:45 +0300

    Add streams and hash tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
+const FILE = "src/streams/files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const ws = fs.createWriteStream(FILE);
+
+    process.stdin.pipe(ws);
+
+    ws.on("error", (err) => {
+      throw `Error:", ${err}`;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
